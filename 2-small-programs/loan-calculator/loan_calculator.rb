95db2267a2f7742f26838453023bc3820c20f109
Scott Hoecker <hoeckers@gmail.com>
2021-08-24 10:53:31 -0500

    Loan calculator in progress

--- 2-small-programs/loan-calculator/loan_calculator.rb
+++ 2-small-programs/loan-calculator/loan_calculator.rb
@@ -1,106 +1,19 @@
-# Get the loan amount, APR, and loan duration from user
-# Enter those values into the formula
-# Print result
-
-require 'yaml'
-MESSAGES = YAML.load_file('messages.yml')
-LANGUAGE = 'en'
-
 def valid_number?(num)
   num.to_i.to_s == num || num.to_f.to_s == num
 end
 
-def messages(message, lang='en')
-  MESSAGES[lang][message]
-end
+puts "Welcome to the loan calculator!"
 
-def prompt(key)
-  message = messages(key, LANGUAGE)
-  puts "=> #{message}"
-end
-
-def code_prompt(string)
-  puts "=> #{string}"
-end
-
-prompt('welcome')
-
-name = ''
 loop do
-  name = gets.chomp
-
-  if name.empty?
-    prompt('valid_name')
-  else
-    break
-  end
+  print "Please enter the loan amount: $"
+  loan_amount = gets.chomp
+  break if valid_number?(loan_amount)
+  puts "Sorry, that doesn't seem like a valid dollar amount.\nPlease try again"\
+      " (you do not need to type the $ sign)"
 end
 
-code_prompt("Hi #{name}!")
-
-loop do # main loop
-  number1 = ''
-  loop do
-    prompt('first_number')
-    number1 = gets.chomp
-
-    if valid_number?(number1)
-      break
-    else
-      prompt('invalid_number')
-    end
-  end
-
-  number2 = ''
-  loop do
-    prompt('second_number')
-    number2 = gets.chomp
-    if valid_number?(number2)
-      break
-    else
-      prompt('invalid_number')
-    end
-  end
-
-  operator_prompt = <<-MSG
-    What operation would you like to perform?
-    1) add
-    2) subtract
-    3) multiply
-    4) divide
-  MSG
-
-  code_prompt(operator_prompt)
-
-  operator = ''
-  loop do
-    operator = gets.chomp
-
-    if %w(1 2 3 4).include?(operator)
-      break
-    else
-      prompt('operator_error')
-    end
-  end
-
-  code_prompt("#{operation_to_message(operator)} the two numbers...")
-
-  result = case operator
-           when '1'
-             number1.to_i() + number2.to_i()
-           when '2'
-             number1.to_i() - number2.to_i()
-           when '3'
-             number1.to_i() * number2.to_i()
-           when '4'
-             number1.to_f() / number2.to_f()
-
-           end
-  code_prompt("The result is #{result}")
-
-  prompt('exit_prompt')
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+loop do
+  print "Please enter the APR: %"
+  apr = gets.chomp
+  break if valid_number?(apr)
 end
-
-prompt('exit_message')
--- 2-small-programs/loan-calculator/pseudocode.md
+++ 2-small-programs/loan-calculator/pseudocode.md
@@ -21,15 +21,73 @@ supplied details about the loan
         - p = loan amount
         - j = monthly interest rate
         - n = loan duration in months
+
+## Implicit Requirements
   
+## Clarifying Questions
 
+  * Should the loan duration support months as well? Or only years?
+  * 
 
+## Mental Model
 
+A user will be prompted for information about the loan (loan amount, APR, loan
+duration), and will then be supplied a monthly payment amount. The APR and loan
+duration will need to be converted from years to months.
 
+## Examples
 
+  ### Example 1
+  * Inputs:
+    * loan_amount = 100000
+    * loan_duration = 10 years
+    * APR = 6%
+  * Outputs:
+    * $1,110.21
+  
+  ### Example 2
+  * Inputs:
+    * loan_amount = $70
+    * loan_duration = 9 months
+    * APR = 2%
+  * Outputs:
+    * $7.84
+
+## Data Structure
+
+Working with a hash could be useful for the loan information, rather than just
+separate variables? The information could be received, processed, then entered
+into the hash. 
+
+## Algorithm
+
+* Ask user for loan amount, save as loan_amount
+* Ask user for APR (as a percent), save as APR:
+  * Divide APR by 100 to convert to decimal
+  * Divide by 12 to get the monthly interest, save as monthly_interest
+* Ask user for the loan duration:
+  * First ask for years, multiply by 12 and save as loan_duration
+  * Then ask for months, add to loan_duration
+* Save result of formula with variables plugged in as monthly_payment
+* Print monthly payment
+
+## Pseudocode
 
 START
-
 GET loan amount SET loan_amount
+SUBRPROCESS verify loan_amount
+
 GET APR SET apr
-GET loan duration SET loan_duration
+SUBPROCESS verify apr
+SET monthly_interest = (apr / 100) / 12
+
+GET loan duration years SET years
+GET loan duration months SET months
+SET loan_duration = (years * 12) + months
+
+SET monthly_payment =  loan_amount * (monthly_interest / (1 - (1 +
+monthly_interest)**(-loan_duration)))
+
+PRINT monthly_payment
+
+END
