00a27c0dd0b793fee1b5a510636f74a696896b1d
Scott Hoecker <hoeckers@gmail.com>
2021-09-04 13:50:37 -0500

    Work on easy 6

---
+++ small-problems-exercises/easy-6/halvsies.rb
@@ -0,0 +1,10 @@
+=begin
+
+Problem:
+Write a method that takes an Array as an argument, and returns two Arrays (as a
+pair of nested Arrays) that contain the first half and second half of the
+original Array, respectively. If the original array contains an odd number of
+elements, the middle element should be placed in the first half Array.
+
+Input: one array
+Output: two arrays, one cont
