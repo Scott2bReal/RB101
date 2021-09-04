fa4d26466a152218cb843d4672126816fc9edd37
Scott Hoecker <hoeckers@gmail.com>
2021-09-04 15:26:36 -0500

    Finish halvsies

--- small-problems-exercises/easy-6/halvsies.rb
+++ small-problems-exercises/easy-6/halvsies.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 =begin
 
 Problem:
@@ -7,4 +9,38 @@ original Array, respectively. If the original array contains an odd number of
 elements, the middle element should be placed in the first half Array.
 
 Input: one array
-Output: two arrays, one cont
+Output: On nested array containing two arrays, the first and second half of the
+        input array
+
+Data Structure: Array
+
+Algorithm:
+  - initialize return array = [[], []]
+  - SET halfway = array.size / 2.0, round up
+  - initialize counter
+  - Until counter == halfway
+    - Remove and add element from input array to array one
+  - Until input array == empty
+    - Remove and add element from input array to array two
+
+=end
+
+def halvsies(array)
+  final_array = [[], []]
+  halfway = (array.size / 2.0).ceil
+  counter = 1
+  until counter > halfway
+    final_array[0] << array.shift
+    counter += 1
+  end
+
+  until array.empty?
+    final_array[1] << array.shift
+  end
+  final_array
+end
+
+puts halvsies([1, 2, 3, 4]) == [[1, 2], [3, 4]]
+puts halvsies([1, 5, 2, 4, 3]) == [[1, 5, 2], [4, 3]]
+puts halvsies([5]) == [[5], []]
+puts halvsies([]) == [[], []]
