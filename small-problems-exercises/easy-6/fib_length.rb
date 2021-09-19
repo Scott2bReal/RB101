5d26f24b5a1bc1f28b87945860ae9fa00ef0da56
Scott Hoecker <hoeckers@gmail.com>
2021-09-19 16:55:45 -0500

    update

--- small-problems-exercises/easy-6/fib_length.rb
+++ small-problems-exercises/easy-6/fib_length.rb
@@ -33,8 +33,8 @@ def find_fibonacci_index_by_length(number)
   counter
 end
 
-puts find_fibonacci_index_by_length(2) == 7          # 1 1 2 3 5 8 13
-puts find_fibonacci_index_by_length(3) == 12         # 1 1 2 3 5 8 13 21 34 55 89 144
+puts find_fibonacci_index_by_length(2) == 7 # 1 1 2 3 5 8 13
+puts find_fibonacci_index_by_length(3) == 12 # 1 1 2 3 5 8 13 21 34 55 89 144
 puts find_fibonacci_index_by_length(10) == 45
 puts find_fibonacci_index_by_length(100) == 476
 puts find_fibonacci_index_by_length(1000) == 4782
