75e88ebc9396da1a29f0db4ff19599fb61bbbe11
Scott Hoecker <hoeckers@gmail.com>
2021-09-20 23:48:44 -0500

    Finish easy 8

---
+++ small-problems-exercises/easy-8/reversed_number.rb
@@ -0,0 +1,30 @@
+=begin
+
+Problem: Write a method that takes a positive integer as an argument and returns
+that number with its digits reversed.
+
+Input: Positive integer
+Output: Integer with digits reversed, ignoring leading zeros
+
+Data Structure: Array
+
+Algorithm:
+  - Initialize final_number
+  - Make array of digits
+  - Reverse array
+  - Join array
+  - Convert to int
+  - return
+
+=end
+
+def reversed_number(num)
+  num.digits.join.to_i
+end
+
+puts reversed_number(12345) == 54321
+puts reversed_number(12213) == 31221
+puts reversed_number(456) == 654
+puts reversed_number(12000) == 21 # No leading zeros in return value!
+puts reversed_number(12003) == 30021
+puts reversed_number(1) == 1
